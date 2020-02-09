correct USDT mislabelling in HitBTC, save averages, and rename consumer to subtractor

bin/aggregator.py
@@ -5,13 +5,19 @@ from os import environ
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 
-def writeBatch(batch, identifier):
+def writeBatch(batch, identifier, table):
     batch \
             .write \
             .mode('append') \
             .format('org.apache.spark.sql.cassandra') \
-            .options(table='trades', keyspace='flucturate') \
+            .options(table=table, keyspace='flucturate') \
             .save()
+
+def writeBatchToTrades(batch, identifier):
+    writeBatch(batch, identifier, 'trades')
+
+def writeBatchToAverages(batch, identifier):
+    writeBatch(batch, identifier, 'averages')
 
 spark = SparkSession.builder.appName('Aggregator').getOrCreate()
 host = f'{environ["KAFKA_MASTER"]}:9092'
@@ -35,21 +41,29 @@ df = df \
         .withWatermark('time', '1 minute')
 allQuery = df \
         .writeStream \
-        .foreachBatch(writeBatch) \
+        .option('checkpointLocation', f'hdfs://{environ["HADOOP_MASTER"]}:9000/ckpt/trades') \
+        .foreachBatch(writeBatchToTrades) \
         .start()
 df = df \
         .groupBy(F.window('time', '1 minute'), 'base', 'quote', 'exchange') \
-        .agg((F.sum(df.price * df.quantity)/F.sum(df.quantity)).alias('value'))
+        .agg((F.sum(df.price * df.quantity)/F.sum(df.quantity)).alias('price')) \
+        .selectExpr('window.start AS start', 'base', 'quote', 'exchange', 'price')
+avgQuery = df \
+        .writeStream \
+        .option('checkpointLocation', f'hdfs://{environ["HADOOP_MASTER"]}:9000/ckpt/averages') \
+        .foreachBatch(writeBatchToAverages) \
+        .start()
 df = df.selectExpr(
-        'CONCAT(window.start, ",", base, ",", quote, ",", exchange) AS key',
-        'CAST(value AS STRING)'
+        'CONCAT(start, ",", base, ",", quote, ",", exchange) AS key',
+        'CAST(price AS STRING) AS value'
         )
 aggQuery = df \
         .writeStream \
+        .option('checkpointLocation', f'hdfs://{environ["HADOOP_MASTER"]}:9000/ckpt/agg') \
         .format('kafka') \
         .option('kafka.bootstrap.servers', host) \
         .option('topic', 'agg') \
-        .option('checkpointLocation', f'hdfs://{environ["HADOOP_MASTER"]}:9000/ckpt/') \
         .start()
 allQuery.awaitTermination()
+avgQuery.awaitTermination()
 aggQuery.awaitTermination()